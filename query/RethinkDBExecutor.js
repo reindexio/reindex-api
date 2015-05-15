770129031b5c91db5ec7a5079a738a828202e06b
Mikhail Novikov <freiksenet@gmail.com>
2015-05-15 14:38:41 +0300

    Removed extra tests.

--- query/RethinkDBExecutor.js
+++ query/RethinkDBExecutor.js
@@ -2,7 +2,7 @@ import RethinkDB from 'rethinkdb';
 
 const RethinkDBExecutor = {
   async executeQuery(conn, app, query) {
-    const reQLQuery = query.toReQL(RethinkDB, RethinkDB.db(app.dbName));
+    const reQLQuery = query.toReQL(RethinkDB.db(app.dbName));
     return await reQLQuery.run(conn);
   },
 };
