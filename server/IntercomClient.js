7a7de3ea314bbb94db275b116149858eac90a843
Ville Immonen <ville.immonen@iki.fi>
2015-10-05 00:58:06 +0300

    Track all admin queries
    This is useful because it lets us see in Intercom which users are
    actively using the product.
    
    At first I only tracked calls to `migrate` because I thought tracking
    everything would add a lot of noise. However, we can just track queries
    made as the admin user, as that typically means queries from CLI or
    GraphiQL – not the queries made in apps of our users.
    
    Also moved the tracking to setImmediate – tracking should not add any
    slowdown to queries.

--- server/IntercomClient.js
+++ server/IntercomClient.js
@@ -2,6 +2,7 @@ import { Client } from 'intercom-client';
 
 import Config from './Config';
 import Monitoring from '../Monitoring';
+import { getIntercomSettings } from '../graphQL/builtins/IntercomSettings.js';
 
 let client;
 const { appId, appApiKey } = Config.get('Intercom');
@@ -17,12 +18,13 @@ function callback(error, response) {
   }
 }
 
-export function trackEvent(userId, eventName, metadata) {
-  if (client) {
+export function trackEvent(credentials, eventName, metadata) {
+  const settings = getIntercomSettings(credentials);
+  if (client && settings) {
     client.events.create({
       created_at: Math.round(Date.now() / 1000),
       event_name: eventName,
-      user_id: userId,
+      user_id: settings.userId,
       metadata,
     }, callback);
   }
