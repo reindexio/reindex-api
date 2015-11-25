474a4ae71f7f22561af11145ea91f5b8a6fa3e67
Ville Immonen <ville.immonen@iki.fi>
2015-11-25 17:25:08 +0200

    Only enable logging metrics in production

--- server/Metrics.js
+++ server/Metrics.js
@@ -1,7 +1,14 @@
-const Metrics = {
-  increment(key, value, source) {
-    console.log(`source=${source} count#${key}=${value}`);
-  },
-};
+let Metrics;
+if (process.env.NODE_ENV === 'production') {
+  Metrics = {
+    increment(key, value, source) {
+      console.log(`source=${source} count#${key}=${value}`);
+    },
+  };
+} else {
+  Metrics = {
+    increment() {},
+  };
+}
 
 export default Metrics;
