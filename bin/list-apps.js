90224414f26218eaa565547b0dbe468f44db89f1
Ville Immonen <ville.immonen@iki.fi>
2016-11-21 23:06:24 +0200

    Fix list-apps script crashing for null fields (#272)
    * Fix list-apps script crashing for null fields
    
    * Remove extra console.log

--- bin/list-apps.js
+++ bin/list-apps.js
@@ -6,9 +6,14 @@ async function main() {
   const table = new CliTable({
     head: ['ID', 'Hostname', 'DB Name', 'Cluster'],
   });
-  apps.forEach((app) => table.push(
-    [app.id, app.hostname, app.database.name, app.database.cluster],
-  ));
+  apps.forEach((app) =>
+    table.push([
+      app.id || '',
+      app.hostname || '',
+      app.database.name || '',
+      app.database.cluster || '',
+    ])
+  );
   console.log(table.toString());
 }
 
